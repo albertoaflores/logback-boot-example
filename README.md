b9e77562af039f167b7622762c7ce824d91aab16
Alberto A. Flores <aaflores@gmail.com>
2017-11-07 19:29:03 -0500

    Added sample tests

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ In this example, we test the following scenarios:
 
 - [x] Different log configuration using Spring Profiles
 - [x] Reusing "included" values in other logback defaults (Spring Boot's defaults)
-- [ ] Test log captures with logback
+- [ ] Test log captures with logback using a TestRule
+- [ ] Test log captures with SLF4J extensions
 - [x] Enable multiple logs in a Spring Boot application
 - [ ] Enable Logback filtering
 - [ ] Coloring
@@ -16,5 +17,6 @@ In this example, we test the following scenarios:
 
 * https://www.slf4j.org/extensions.html
 * https://logback.qos.ch/manual/filters.html
+* https://gist.github.com/geowarin/3685379c1ae5f100a02c
 
 
--- src/test/java/io/cybertech/examples/logbackExample/LogbackExampleApplicationTests.java
+++ src/test/java/io/cybertech/examples/logbackExample/LogbackExampleApplicationTests.java
@@ -1,16 +1,23 @@
 package io.cybertech.examples.logbackExample;
 
+import io.cybertech.examples.logbackExample.controller.SampleController;
+import io.cybertech.examples.logbackExample.model.PizzaOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest()
 public class LogbackExampleApplicationTests {
+    @Autowired  private SampleController controller;
 
 	@Test
 	public void contextLoads() {
+        PizzaOrder order = new PizzaOrder();
+
+		controller.placeOrder(order);
 	}
 
 }
